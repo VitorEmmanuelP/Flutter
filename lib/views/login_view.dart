c71f7a4756b62cdae314791640f4889220eddc6d
Vitor Emmanuel <vitoremmanuel520@gmail.com>
2023-03-14 13:05:29 -0300

    second commit

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -56,7 +56,8 @@ class _LoginViewState extends State<LoginView> {
                 final userCredential = await FirebaseAuth.instance
                     .signInWithEmailAndPassword(
                         email: email, password: password);
-                print(userCredential);
+                Navigator.of(context)
+                    .pushNamedAndRemoveUntil('/notes/', (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   print('User not fould');
